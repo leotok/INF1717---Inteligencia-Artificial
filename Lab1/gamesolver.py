fcc58a39c9e81ad6e41db09ce7254097aa199ce8
leotok <leo.wajnsztok@gmail.com>
2016-03-21 19:24:40 -0300

    funfando

--- Lab1/gamesolver.py
+++ Lab1/gamesolver.py
@@ -1,92 +1,101 @@
-# -*- coding: utf-8 -*-
-
 ### LIBRARIES:
 
 from collections import deque
 
 ### SETUP:
 
-ROOT = "C:\\Users\\e1312737.LAB-GRAD\\Documents\\ia161\\" 
+ROOT = "C:\\Users\\e1312737.LAB-GRAD\\Documents\\ia161\\"
+ROOT_Leo = "/Users/LeoWajnsztok/Documents/Python/INF1771/Lab1/"
 
-file = open((ROOT + "maze41.txt"), "r+") # read table file
+file = open(( "maze41.txt"), "r+")
 
-table = file.read() # tabuleiro
+table = file.read()
 
 ### PROCESS:
 
 table = table.split('\n')
+# copia table pra table_percorrida que ira imprimir a matriz com o caminho
+table_percorrida = map(list,table)
 
+
+# Funcao para pegar os vizinhos de um no da matriz que nao estao bloqueados com 'X'
 
 def pegaVizinhos(tupla, arq):
-    
+
     run = list()
-    
-    takedown = list()
-    
+
+    vizinhos = list()
+
     run.append([tupla[0], tupla[1]+1])
     run.append([tupla[0], tupla[1]-1])
     run.append([tupla[0]+1, tupla[1]])
     run.append([tupla[0]-1, tupla[1]])
-              
-    for i in run: 
-        if arq[i[0]][i[1]] == '.' and i[0] >= 0 and i[1] >= 0:
-            takedown.append([i[0], i[1]])
-               
-    return(takedown)
-    
+
+    for i in run:
+        if ( arq[i[0]][i[1]] == '.' or arq[i[0]][i[1]] == 'F') and i[0] >= 0 and i[1] >= 0:
+            vizinhos.append([i[0], i[1]])
+
+    return(vizinhos)
+
+# Funcao para achar a posicao final no Maze representado por 'F'
+
 def findEnd(arq):
-    
+
     for i in range(1, len(arq)):
         for j in range(1, len(arq[i])):
-            
+
             if arq[i][j] == 'F':
-                
-                return(i,j)
- 
+
+                return[i,j]
+
 
 end = findEnd(table)
-   
+print 'End: ' + str(end)
+
+# Funcao para achar a posicao inicial no Maze representado por 'I'
+
 def findStart(arq):
-    
+
     for i in range(1, len(arq)):
         for j in range(1, len(arq[i])):
-            
+
             if arq[i][j] == 'I':
-                
+
                 return(i,j)
-                
+
 start = findStart(table)
-   
+
+# Funcao para executar a busca em largura e achar o caminho de 'I' a 'F'
+
 def treeSearch(start):
     fila = deque()
-    
+
     visited = list()
-        
-    fila.append(start) 
-        
+
+    fila.append(start)
+
     while len(fila) > 0:
         valorAtual = fila.popleft()
         visited.append(valorAtual)
-        
-        if(table[valorAtual[0]][valorAtual[1]] != '.'):
-            print(table[valorAtual[0]][valorAtual[1]])
-        
-        
-        if (valorAtual == end):
+
+        if valorAtual == end:
             print('got it')
             return 1
-            
-        
-    
+
+        table_percorrida[valorAtual[0]][valorAtual[1]] = 'o'
+
         vizinhos = pegaVizinhos(valorAtual, table)
-        
+
         for vizinho in vizinhos:
-            
+
             try:
                 visited.index(vizinho)
-            
+
             except:
                 fila.append(vizinho)
+    return 0
 
-            
-    
+print treeSearch(start)
+
+for line in table_percorrida:
+    print line
